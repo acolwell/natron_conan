87e15c2443d7cb87e9f8bab8c2da70c75580c669
Aaron Colwell <300262+acolwell@users.noreply.github.com>
2025-03-02 17:33:26 -0800

    don't build runtimes on Windows.

--- recipes/clang/all/conanfile.py
+++ recipes/clang/all/conanfile.py
@@ -42,7 +42,6 @@ class ClangConanfile(ConanFile):
             "LLVM_INSTALL_UTILS": "ON",
             "LLVM_ENABLE_PROJECTS": "clang;clang-tools-extra",
             "LLVM_TARGETS_TO_BUILD": "host;AArch64;ARM;X86",
-            "LLVM_ENABLE_RUNTIMES": "all",
             "LLVM_ENABLE_EH": "ON",
             "LLVM_ENABLE_RTTI": "ON" }
 
@@ -51,11 +50,11 @@ class ClangConanfile(ConanFile):
         else:
             config_vars["LLVM_BUILD_LLVM_DYLIB"] = "ON"
             config_vars["LLVM_LINK_LLVM_DYLIB"] = "ON"
+            config_vars["LLVM_ENABLE_RUNTIMES"] = "all",
 
         cmake.configure(variables=config_vars, build_script_folder="llvm")
         cmake.build()
 
-       
     def package(self):
         cmake = CMake(self)
         cmake.install()
